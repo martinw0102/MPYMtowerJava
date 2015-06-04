960403fcb83218b87c5bf6fe8f38c73ddf655459
Matardarix <carliermathieu.e@gmail.com>
2015-06-04 17:01:36 +0200

    Ajout du package Affichage

---
+++ ExiaGeek/src/fr/Affichage/PlateauCase.java
@@ -0,0 +1,11 @@
+package fr.Affichage;
+
+import java.awt.Image;
+
+/**
+ * Interface pour l'affichage d'une case.
+ * @author matardarix
+ */
+public interface PlateauCase {
+	public Image getImage();
+}
---
+++ ExiaGeek/src/fr/Affichage/PlateauPiece.java
@@ -0,0 +1,11 @@
+package fr.Affichage;
+
+/**
+ * Interface pour l'affichage d'une pièce
+ * @author matardarix
+ * @see {@link PlateauCase} 
+ */
+public interface PlateauPiece extends PlateauCase{
+	public int getPositionHorizontale();
+	public int getPositionVerticale();
+}
