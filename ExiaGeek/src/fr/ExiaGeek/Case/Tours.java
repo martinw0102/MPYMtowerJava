92440716c90769ff11716a0c924f495427737b3f
Matardarix <carliermathieu.e@gmail.com>
2015-06-09 11:16:03 +0200

    modification

--- ExiaGeek/src/fr/ExiaGeek/Case/Tours.java
+++ ExiaGeek/src/fr/ExiaGeek/Case/Tours.java
@@ -4,8 +4,20 @@ import fr.ExiaGeek.Partie;
 
 public abstract class Tours extends Entite {
 
+	protected int attReseau;
+	protected int attSysteme;
+	protected int vitAttaque;
+	protected int cout;
+	protected int niveau;
+	
 	public Tours(Partie partie, char dessin, String image, int x, int y) {
 		super(partie, dessin, image, x, y);
 	}
 
+	protected void attaquer(Assaillants assaillant){
+		if(assaillant.estVivant()){
+			assaillant.prendreDommage(this.attReseau);
+			assaillant.prendreDommage(this.attSysteme);
+		}
+	}
 }
