46e94fd2dedab6f0e1d344e31830592ba6df8b0b
yassine <yassine@10.162.128.39>
2015-06-09 10:08:42 +0200

    eh boom

--- ExiaGeek/src/fr/ExiaGeek/Partie.java
+++ ExiaGeek/src/fr/ExiaGeek/Partie.java
@@ -1,10 +1,16 @@
 package fr.ExiaGeek;
 
+
 public class Partie {
 	public int score;
 	public int ressource;
 	public String pseudo;
-	public int nbVague;
-	
-	
+	public int nbVague;	 
+
+	public void calculerScore() {
+		
+		
+	}
 }
+
+		
