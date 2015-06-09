2f907f2d79c587e603e135e6d2d2eb4565d8e048
Nocte <Nocte@10.162.129.2>
2015-06-09 11:53:55 +0200

    blue screen et 404

--- ExiaGeek/src/fr/ExiaGeek/Case/BlueScreen.java
+++ ExiaGeek/src/fr/ExiaGeek/Case/BlueScreen.java
@@ -6,6 +6,14 @@ public class BlueScreen extends Assaillants {
 
 	public BlueScreen(Partie partie, char dessin, String image, int x, int y) {
 		super(partie, dessin, image, x, y);
+		
+		this.pv = 60;
+		this.portee = 0;
+		this.attaque = 35;
+		this.vitDeplacement = 3;
+		this.resistReseau = 0;
+		this.butin = 55;
 	}
+	
 
 }
--- ExiaGeek/src/fr/ExiaGeek/Case/ErreurHTTP404.java
+++ ExiaGeek/src/fr/ExiaGeek/Case/ErreurHTTP404.java
@@ -6,6 +6,13 @@ public class ErreurHTTP404 extends Assaillants {
 
 	public ErreurHTTP404(Partie partie, char dessin, String image, int x, int y) {
 		super(partie, dessin, image, x, y);
+		 
+		this.pv = 35;
+		this.portee = 0;
+		this.attaque = 10;
+		this.vitDeplacement = 1;
+		this.resistSysteme = 0;
+		this.butin = 20;
 	}
 
 }
