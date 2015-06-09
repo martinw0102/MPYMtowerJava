c899fcc4c771a96d1b478363883d87b2e8e01146
Martin <Martin@10.162.128.244>
2015-06-09 09:17:24 +0200

    Tours

---
+++ ExiaGeek/src/fr/ExiaGeek/Case/Tours.java
@@ -0,0 +1,11 @@
+package fr.ExiaGeek.Case;
+
+import fr.ExiaGeek.Partie;
+
+public abstract class Tours extends Entite {
+
+	public Tours(Partie partie, char dessin, String image, int x, int y) {
+		super(partie, dessin, image, x, y);
+	}
+
+}
