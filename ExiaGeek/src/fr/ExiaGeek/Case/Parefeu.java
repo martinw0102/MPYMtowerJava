04aff70c31362d9aa0a50b3a3f70c4b0a83e50d9
yassine <yassine@10.162.128.39>
2015-06-09 11:43:06 +0200

    Stats des tours

--- ExiaGeek/src/fr/ExiaGeek/Case/Parefeu.java
+++ ExiaGeek/src/fr/ExiaGeek/Case/Parefeu.java
@@ -6,6 +6,13 @@ public class Parefeu extends Tours {
 
 	public Parefeu(Partie partie, char dessin, String image, int x, int y) {
 		super(partie, dessin, image, x, y);
+		this.pv = 100;
+		this.cout = 80;
+		this.portee = 0;
+		this.attReseau = 20;
+		this.attSysteme = 0;
+		this.vitAttaque = 2;
+		this.niveau = 1;
 	}
 
 }
