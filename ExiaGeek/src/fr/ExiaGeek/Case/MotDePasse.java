14e57cb11cd9353710b28b5efac4c599216db4cd
Martin <Martin@10.162.128.244>
2015-06-09 09:23:34 +0200

    Classes filles tours

---
+++ ExiaGeek/src/fr/ExiaGeek/Case/MotDePasse.java
@@ -0,0 +1,11 @@
+package fr.ExiaGeek.Case;
+
+import fr.ExiaGeek.Partie;
+
+public class MotDePasse extends Tours {
+
+	public MotDePasse(Partie partie, char dessin, String image, int x, int y) {
+		super(partie, dessin, image, x, y);
+	}
+
+}
