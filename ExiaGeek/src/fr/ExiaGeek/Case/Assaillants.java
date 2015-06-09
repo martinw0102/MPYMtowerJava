1d1236d541a30a93440fa5f4a8b269fd9172b323
Martin <Martin@10.162.128.244>
2015-06-09 10:53:26 +0200

    Assaillants up

--- ExiaGeek/src/fr/ExiaGeek/Case/Assaillants.java
+++ ExiaGeek/src/fr/ExiaGeek/Case/Assaillants.java
@@ -3,6 +3,19 @@ package fr.ExiaGeek.Case;
 import fr.ExiaGeek.Partie;
 
 public abstract class Assaillants extends Entite {
+	
+	protected int attaque;
+	protected int vitDeplacement;
+	protected int resistReseau;
+	protected int resistSysteme;
+	protected int butin;
+	
+	protected void seDeplacer() {
+		
+	}
+	protected void attaquer() {
+		
+	}
 
 	public Assaillants(Partie partie, char dessin, String image, int x, int y) {
 		super(partie, dessin, image, x, y);
