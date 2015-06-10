aef323a32b4c4da3d5cb2da041259d9da2737f1d
Martin <Martin@10.162.128.244>
2015-06-10 09:20:56 +0200

    e

--- ExiaGeek/src/fr/ExiaGeek/ExiaGeek.java
+++ ExiaGeek/src/fr/ExiaGeek/ExiaGeek.java
@@ -1,14 +1,37 @@
 package fr.ExiaGeek;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 import fr.ExiaGeek.BDD.*;
 
 public class ExiaGeek {
 	
 	public static void main(String[] args) {
 		Lanceur launcher = new Lanceur();
-		BDDConnexion bdd = new BDDConnexion();
+		try {
+
+			File file = new File("C:\\Users\\Martin\\git\\MPYMtowerJava\\ExiaGeek\\src\\fr\\ExiaGeek\\BDD\\Deorro x Chris Brown - Five More Hours.mp3");
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			
+			try {
+				Player player = new Player(bis);
+				player.play();
+			}catch(JavaLayerException ex){
+				
+			}
+		}catch(IOException e){
+			
+		}
+		/*BDDConnexion bdd = new BDDConnexion();
 		bdd.open();
 		bdd.insertTest();
-		bdd.close();
+		bdd.close();*/
+		}
 	}
-}
+
