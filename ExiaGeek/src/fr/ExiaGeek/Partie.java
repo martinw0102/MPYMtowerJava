02c771f19998c3d3f51822edd5d6823688a2e374
Matardarix <carliermathieu.e@gmail.com>
2015-06-10 15:57:39 +0200

    Ajout de l'affichage console

--- ExiaGeek/src/fr/ExiaGeek/Partie.java
+++ ExiaGeek/src/fr/ExiaGeek/Partie.java
@@ -1,11 +1,19 @@
 package fr.ExiaGeek;
 
+import java.util.ArrayList;
+
 import fr.ExiaGeek.Affichage.Plateau;
 import fr.ExiaGeek.Case.Case;
 import fr.ExiaGeek.Case.CaseVide;
+import fr.ExiaGeek.Case.Entite;
 
 public class Partie {
+	private final static int HAUTEUR = 25;
+	private final static int LARGEUR = 25;
+	
 	private final Case cases[][];
+	//private final ArrayList<Entite> entites;
+	private final Boolean affichageConsole = false;
 	private Plateau plateau;
 	
 	public int ressource;
@@ -14,19 +22,55 @@ public class Partie {
 	private static int score = 0;
 
 	public Partie() {
-		this.cases = new Case[25][25];
+		this.cases = new Case[HAUTEUR][LARGEUR];
 		Case uneCase;
-		for(int y = 0; y < 25; y++){
-			for(int x = 0; x < 25; x++){
+		for(int y = 0; y < HAUTEUR; y++){
+			for(int x = 0; x < LARGEUR; x++){
 				uneCase = new CaseVide();
 				this.setXY(x, y, uneCase);
 			}
 		}
-		this.plateau = new Plateau(25,25, this.cases);
+		this.plateau = new Plateau(HAUTEUR, LARGEUR, this.cases);
 	}
 	
 	public void afficher(){
+		if(this.affichageConsole){
+			this.afficherConsole();
+		}else{
+			this.afficherGraphique();
+		}
+	}
+	
+	private void afficherConsole(){
+		for(int y = 0; y < HAUTEUR; y++){
+			for(int x = 0; x < LARGEUR; x++){
+				final Entite e = this.getEntite(x, y);
+				if(e == null){
+					System.out.println(this.getXY(x, y).getDessin());
+				}else{
+					System.out.println(e.getDessin());
+				}
+			}
+		}
+	}
+	
+	private void afficherGraphique(){
 		this.plateau.rafraichir();
+	}
+	
+	public Entite getEntite(final int x, final int y){
+		for(final Entite e : this.entites){
+			if(e != null){
+				if((e.getX() == x) && (e.getY() == y)){
+					return e;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public Case getXY(final int x, final int y){
+		return this.cases[x][y];
 	}
 	
 	public void setXY(final int x, final int y, final Case c) {
@@ -34,19 +78,19 @@ public class Partie {
 	}
 	
 	public void calculerScore() {
+	
+		int TempsEnMillisecondes = 1000;	
 
-		    	int TempsEnMillisecondes = 1000;
-
-		    		for (int i = 0; i <= 10; i++) {
-		    			try {
-		    	                Thread.sleep(TempsEnMillisecondes);
-		    	                Partie.score++;
-		    	        } 
-		    	        catch (InterruptedException e) {
-		    	        }
-		    	            System.out.println(Partie.score);
-		    	            
-		    	    }
+		for (int i = 0; i <= 10; i++) {
+			try {
+	                Thread.sleep(TempsEnMillisecondes);
+	                Partie.score++;
+	        } 
+	        catch (InterruptedException e) {
+	        }
+	            System.out.println(Partie.score);
+	            
+	    }
 	}
 
 	public static int getScore() {
