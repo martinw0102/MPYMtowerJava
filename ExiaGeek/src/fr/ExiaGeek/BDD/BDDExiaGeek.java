e7dd0af517cfe8596703ce2a6358800baf26b87d
Matardarix <carliermathieu.e@gmail.com>
2015-06-11 09:10:41 +0200

    Réagencement #designer

--- ExiaGeek/src/fr/ExiaGeek/BDD/BDDExiaGeek.java
+++ ExiaGeek/src/fr/ExiaGeek/BDD/BDDExiaGeek.java
@@ -1,11 +1,11 @@
 package fr.ExiaGeek.BDD;
 
-
 public class BDDExiaGeek {
 	
 	public static String getQueryInsertPartie(int score) {
 		return "INSERT INTO `partie` (`Score`, `PV_PosteDeTravail`, `PO`, `ID_Carte`)   VALUES ('"+ score +"', '"+ 250 +"', '"+ 7895 +"', '"+ 1 +"')";
 	}
+	
 	public static String getQuerySelect(final int id) {
 		return "SELECT * FROM `chemin` WHERE `ID_Carte` = " + id;
 	}
